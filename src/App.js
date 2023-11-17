2e452d5ede7c094f83e55d2c629c5d90d72b3ad2
Fábián Levente <97455713+fablevi@users.noreply.github.com>
2023-11-17 18:47:49 +0100

    sps

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Szeretem Pali Szimonettát
         </a>
       </header>
     </div>
