f664ca5a0378292cb1405f3762a14c344042c7a5
Fábián Levente <97455713+fablevi@users.noreply.github.com>
2024-01-31 15:44:52 +0100

    whoami

--- src/Components/Left/HowAmI.jsx
+++ src/Components/Left/HowAmI.jsx
@@ -1,10 +1,15 @@
-function HowAmI(){
+function HowAmI() {
 
 
-    
-    return <>
+
+    return <div>
         <h1>How Am I?</h1>
-    </>
+
+        <div className="whoami">Hi! I'm Levi, a programmer.</div>
+        <div className="whoami">I live in Budapest, the capital of Hungary. </div>
+        <div className="whoami">I started working in 2022, in 2020 I became interested in programming, backend and frontend development, and more recently AI has caught my attention.</div>
+        <div className="whoami">You can read more about me below 🙂</div>
+    </div>
 }
 
 export default HowAmI;
