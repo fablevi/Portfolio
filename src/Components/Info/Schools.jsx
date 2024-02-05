b9881031a237090378342f4b30aa40e8a92fcc73
Fábián Levente <97455713+fablevi@users.noreply.github.com>
2024-02-05 13:11:32 +0100

    New items on schools.jsx

--- src/Components/Info/Schools.jsx
+++ src/Components/Info/Schools.jsx
@@ -1,4 +1,5 @@
 import { Col, Row, Container, Image } from "react-bootstrap";
+import { TagGroup, Item } from '@adobe/react-spectrum'
 
 import SchoolJSON from "../json/Schools.json";
 
@@ -29,7 +30,31 @@ function Schools(props) {
                             <Col className="schoolIMGDiv" xs={12} xxl={7}>
                                 <Image src={images[index]} rounded style={{ marginBottom: "2%" }} />
                                 <div className="schoolIMGFlexBox">
-                                    <div className="schoolIMGFlexBoxDivs">
+                                    <TagGroup UNSAFE_className={props.componentScheme === "light" ? "lightItems" : "darkItems"} zIndex={0}>
+                                        <Item>{data.city}</Item>
+                                        <Item>{data.learned}</Item>
+                                        <Item>{data.paper}</Item>
+                                        <Item>{data.when}</Item>
+                                        <Item target="_blank" href={data.website}>Website</Item>
+                                        <Item target="_blank" href={data.google_maps}>On Maps</Item>
+                                    </TagGroup>
+                                </div>
+                            </Col>
+                            <Col className="schoolShortListDiv" xs={12} xxl={5}>
+                                {data.short_list}
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+        })}
+    </>
+}
+
+export default Schools;
+
+/*
+ <div className="schoolIMGFlexBoxDivs">
                                         <Row>
                                             <Col><MapIcon /></Col>
                                             <Col style={{ paddingLeft: 0 }}><div>{data.city}</div></Col>
@@ -65,17 +90,4 @@ function Schools(props) {
                                             <Col style={{ paddingLeft: 0 }}><div>{data.google_maps}</div></Col>
                                         </Row>
                                     </div>
-                                </div>
-                            </Col>
-                            <Col className="schoolShortListDiv" xs={12} xxl={5}>
-                                {data.short_list}
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
-            </div>
-        })}
-    </>
-}
-
-export default Schools;
+*/
